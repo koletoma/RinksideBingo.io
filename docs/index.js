f2bfc8911aa65ecb7d89b75bf969a383393b6100
Kole Toma <112729864+koletoma@users.noreply.github.com>
2024-01-27 13:11:45 -0700

    shuffling

--- docs/index.js
+++ docs/index.js
@@ -1,6 +1,6 @@
-Shuffle();
+MixItUp();
 
-function Shuffle() {
+function MixItUp() {
     //24 tiles required to be shuffled and placed into table upon page refresh
     tileTextArray = [
         "Home Goal", "Away Goal", "Face-Off Violation", "One-Timer", "Hat-Trick",
@@ -11,7 +11,7 @@ function Shuffle() {
     ];
     
     var stack = [];
-    tileTextArray.Shuffle.forEach((text)=>{
+    shuffle(tileTextArray).forEach((text)=>{
         stack.push(text);
     })
 
@@ -22,3 +22,21 @@ function Shuffle() {
     });
     
 }
+
+function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle.
+    while (currentIndex > 0) {
+  
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
