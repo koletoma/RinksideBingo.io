d6e5e5195c91d7839538561a1a0a3f65c60b52dc
Kole Toma <112729864+koletoma@users.noreply.github.com>
2024-01-27 13:08:32 -0700

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -2,11 +2,23 @@ Shuffle();
 
 function Shuffle() {
     //24 tiles required to be shuffled and placed into table upon page refresh
-    tiles = [
+    tileTextArray = [
         "Home Goal", "Away Goal", "Face-Off Violation", "One-Timer", "Hat-Trick",
         "High Stick", "Icing", "Power Play Goal", "Penalty Kill", "Tripping",
         "Overtime", "Player Blocks Shot", "Blood!", "Offside", "Puck In Stands",
         "Glove Save", "Gloves Dropped", "Ref Falls Down", "Fans Boo", "Lost Fan",
         "Same Number Different Player", "Player Exits Game Early", "Broken Stick", "K/O"
     ];
+    
+    var stack = [];
+    tileTextArray.Shuffle.forEach((text)=>{
+        stack.push(text);
+    })
+
+    var tilesArray = document.querySelectorAll('table td');
+
+    tilesArray.forEach((tile)=>{
+        tile.innerHTML = stack.pop();
+    });
+    
 }
