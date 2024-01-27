7c0dd6fd902893a464e67776638c6afb95a8ab6d
Kole Toma <112729864+koletoma@users.noreply.github.com>
2024-01-27 13:19:55 -0700

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -15,7 +15,7 @@ function MixItUp() {
         stack.push(text);
     })
 
-    var tilesArray = document.querySelectorAll("td");
+    var tilesArray = document.querySelectorAll("#tile");
 
     tilesArray.forEach((tile)=>{
         tile.innerHTML = stack.pop();
