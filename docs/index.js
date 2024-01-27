1a37ca0fb5ffe066f2df3d25fec88aa1af25fe77
Kole Toma <112729864+koletoma@users.noreply.github.com>
2024-01-27 12:48:13 -0700

    added tile names and shuffle btn

--- docs/index.js
+++ docs/index.js
@@ -1,3 +1,14 @@
 $(()=>{
-    console.log("JS is working");
+    Shuffle();
 });
+
+function Shuffle() {
+    //24 tiles required to be shuffled and placed into table upon page refresh
+    tiles = [
+        "Home Goal", "Away Goal", "Face-Off Violation", "One-Timer", "Hat-Trick",
+        "High Stick", "Icing", "Power Play Goal", "Penalty Kill", "Tripping",
+        "Overtime", "Player Blocks Shot", "Blood!", "Offside", "Puck In Stands",
+        "Glove Save", "Gloves Dropped", "Ref Falls Down", "Fans Boo", "Lost Fan",
+        "Same Number Different Player", "Player Exits Game Early", "Broken Stick", "K/O"
+    ];
+}
